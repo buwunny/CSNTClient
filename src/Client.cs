3b679715d7616a3cad76cd938b29ade2d5c6428d
Bunny <rortiz6868@gmail.com>
2024-04-15 17:32:18 -0400

    connected to NetworkTables

---
+++ src/Client.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.WebSockets;
+
+namespace DNT4
+{
+  public class Client
+  {
+    private readonly int port = 5810; // Default NT4 port
+    private readonly string ip = "localhost"; // Default NT4 address
+    private readonly string name = "Client";
+    private ClientWebSocket? client;
+
+    public Client(string ip, string name)
+    {
+      this.ip = ip;
+      this.name = name;
+    }
+
+    public void Connect()
+    {
+      Console.WriteLine("Connecting to server...");
+      client = new ClientWebSocket();
+      client.Options.AddSubProtocol("networktables.first.wpi.edu");
+      var address = $"ws://{ip}:{port}/nt/{name}";
+      try{
+        client.ConnectAsync(new Uri(address), default).Wait();
+      }
+      catch (Exception e){
+        Console.WriteLine($"Failed to connect to server: {e.Message}");
+        return;
+      }
+      Console.WriteLine("Connected to server.");
+    }
+
+    public bool IsConnected => client?.State == WebSocketState.Open;
+  }
+}
--- src/TestClient.cs
+++ src/TestClient.cs
@@ -1,8 +1,23 @@
-
+using DNT4;
 class TestClient {
 	public static void Main(string[] args)
 	{
-	
+		// Create a client with a specific IP address
+		var client = new DNT4.Client("127.0.0.1", "Test");
+
+		// Connect the client
+		client.Connect();
+		while (client.IsConnected)
+		{
+			// Print the connection status
+			Console.WriteLine("IsConnected: " + client.IsConnected);
+
+			// Wait for half a second
+			Thread.Sleep(500);
+		}
+		// Keep the console open until a key is pressed
+		//Console.WriteLine("Press any key to exit...");
+		//Console.ReadKey();
 	}
 
 }
